8ad33349fcb1b5af7cd2e631e32662aa994d0546
Tom Artiom Fiodorov <tom@glassnode.com>
2020-05-07 22:59:09 +0100

    adding sort

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/afiodorov/radixmmap
+
+go 1.14
+
+require (
+	github.com/edsrzf/mmap-go v1.0.0
+	github.com/twotwotwo/sorts v0.0.0-20160814051341-bf5c1f2b8553
+	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	mm "github.com/edsrzf/mmap-go"
+	"github.com/twotwotwo/sorts"
+)
+
+type Lines []mm.MMap
+
+func (s Lines) Less(i, j int) bool {
+	limI, limJ := 20, 20
+
+	if len(s[i]) < limI {
+		limI = len(s[i])
+	}
+
+	if len(s[j]) < limJ {
+		limJ = len(s[j])
+	}
+
+	return bytes.Compare(s[i][:limI], s[j][:limJ]) == -1
+}
+
+func (s Lines) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s Lines) Len() int {
+	return len(s)
+}
+
+func (s Lines) Key(i int) []byte {
+	l := 20
+	if len(s[i]) < l {
+		l = len(s[i])
+	}
+
+	return s[i][:l]
+}
+
+func (s Lines) Sort() { sorts.ByBytes(s) }
+
+func main() {
+	fileName := flag.String("s", "", "file to sort")
+	flag.Parse()
+
+	file, err := os.Open(*fileName)
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatalf("%v\n", err)
+		}
+	}()
+
+	m, err := mm.Map(file, mm.RDONLY, 0)
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+
+	defer func() {
+		if err := m.Flush(); err != nil {
+			log.Fatalf("%v\n", err)
+		}
+	}()
+
+	var lines Lines
+
+	start := 0
+
+	for i := 0; i < len(m); i++ {
+		if m[i] == byte(10) {
+			lines = append(lines, m[start:i])
+			start = i + 1
+		}
+	}
+
+	if len(m) > start {
+		lines = append(lines, m[start:len(m)])
+	}
+
+	lines.Sort()
+
+	for _, l := range lines {
+		fmt.Println(string(l))
+	}
+}
